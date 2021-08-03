04d09d623eb55cc4dfb54302bde9824992459fdd
Roter-S <58897438+Roter-S@users.noreply.github.com>
2021-08-03 08:35:35 -0600

    inicio

---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+use App\Http\Controllers\UserController;
+
+Route::get('/', [UserController::class,'index']);
+Route::post('users', [UserController::class, 'store'])->name('users.store');
+Route::delete('users/{user}', [UserController::class,'destroy'])->name('users.destroy');
